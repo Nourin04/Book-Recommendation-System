ac66a40a7b98eddf818e0499209b0f0c82ce2390
Noureen AC <nourinnn1823@gmail.com>
2025-02-16 12:15:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ TITLE_FILE = "book_titles.pkl"
 EMBEDDINGS_FILE = "embeddings.pkl"
 THUMBNAIL_FILE = "books.csv"  # Ensure this CSV has 'title' and 'thumbnail' columns
 
+st.image("https://books.google.com/books/content?id=KQZCPgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api", width=100)
+
+
 # Load FAISS index
 @st.cache_resource
 def load_faiss_index():
